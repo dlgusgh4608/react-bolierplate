8d996299e465f450e9ba2d5b84447d748b850dc4
dlgusgh4608 <dlgusgh4608@gamil.com>
2021-01-05 17:21:06 +0900

    add: dark mode

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../../styles/global-style";
+import Components from "./Components";
+import { mode } from "../../styles/theme";
 
 const App = () => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const theme = themeMode === "light" ? mode.light : mode.dark;
+
+  const toggleTheme = () => {
+    setThemeMode(themeMode === "light" ? "dark" : "light");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <button onClick={toggleTheme}>밤낮</button>
+      <GlobalStyle />
+      <Components />
+    </ThemeProvider>
   );
 };
 
